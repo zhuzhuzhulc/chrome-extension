53ac5d14a75ce88756fb0197137aa931aedfd84e
Justin Poliey <justin.d.poliey@gmail.com>
2015-09-18 12:04:25 -0400

    Record live streams remotely
    Instead of requesting and building blobs in memory to upload,
    the client tells a transcoder to concatenate segments.

--- src/browser_action/browser_action.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', onReady, false);
-
-function onReady() {
-  document.querySelector('.save-clip').onclick = function() {
-    chrome.tabs.getSelected(null, function(tab) {
-      chrome.runtime.sendMessage({ message: 'save-clip', tabTitle: tab.title, tabId: tab.id, url: tab.url });
-    });
-  }
-}
